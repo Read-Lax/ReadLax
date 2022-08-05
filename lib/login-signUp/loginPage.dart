33d843d4da8cb172967802a8b0208388413ea048
0RaMsY0 <0ramsy0@gmail.com>
2022-08-05 23:31:34 +0100

    remaking the ui for login screen

--- lib/login-signUp/loginPage.dart
+++ lib/login-signUp/loginPage.dart
@@ -34,145 +34,156 @@ class _LoginPageState extends State<LoginPage> {
         ? Loading()
         : Scaffold(
             resizeToAvoidBottomInset: false,
-            body: Center(
-              child: Container(
-                margin: const EdgeInsets.all(40),
-                padding: const EdgeInsets.all(40),
-                // width: 300,
-                // height: 350,
-                decoration: BoxDecoration(
+            body: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                CircleAvatar(
+                  backgroundImage: Image.asset("assets/app_logo.png").image,
+                  backgroundColor: Colors.grey.withOpacity(0.3),
+                  radius: 50,
+                ),
+                Text(
+                  "Readlax",
+                  style: TextStyle(
+                    fontFamily: "VareLaRound",
+                    fontWeight: FontWeight.bold,
+                    fontSize: 30,
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(30),
+                  padding: const EdgeInsets.all(40),
+                  decoration: BoxDecoration(
                     border: Border.all(),
                     borderRadius: const BorderRadius.all(
                       Radius.elliptical(20, 20),
                     ),
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Colors.black12,
-                          offset: Offset(5.0, 5.0),
-                          blurRadius: 0.0,
-                          spreadRadius: 0.0),
-                      BoxShadow(
-                          color: Colors.black12,
-                          offset: Offset.zero,
-                          blurRadius: 0.0,
-                          spreadRadius: 0.0),
-                    ]),
-                child: Column(mainAxisSize: MainAxisSize.min, children: [
-                  Text(
-                    "Login Page",
-                    style: TextStyle(
-                      color: Colors.blueAccent[400],
-                      fontSize: 30,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: "VareLaRound",
-                      // letterSpacing: 2
+                    gradient: LinearGradient(
+                      colors: [
+                        Colors.green,
+                        Colors.greenAccent,
+                        Colors.blueAccent,
+                      ],
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
                     ),
                   ),
-                  const SizedBox(height: 20),
-                  // gmail text feild
-                  TextField(
-                    // obscureText: true,
-                    autofocus: true,
-                    controller: userGmail,
-                    // cursorColor: Colors.black,
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      label: Text(
-                        "Gmail",
-                        style: TextStyle(
-                          fontFamily: "VareLaRound",
-                          fontWeight: FontWeight.bold,
-                          // color: Colors.black45,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  // User's password text feild
-                  TextField(
-                    // obscureText: true,
-                    controller: userPassword,
-                    // cursorColor: Colors.black,
-                    decoration: const InputDecoration(
-                      fillColor: Colors.black,
-                      border: OutlineInputBorder(
-                          borderRadius:
-                              BorderRadius.all(Radius.circular(10.0))),
-                      label: Text(
-                        "Password",
-                        style: TextStyle(
-                          fontFamily: "VareLaRound",
-                          fontWeight: FontWeight.bold,
-                          // color: Colors.black45,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Text.rich(TextSpan(children: [
-                    const TextSpan(
-                      text: "Don't have an Account?",
-                      style: TextStyle(
-                        fontFamily: "VareLaRound",
-                        fontSize: 12,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    TextSpan(
-                        text: " SingUp",
-                        style: const TextStyle(
-                          fontFamily: "VareLaRound",
-                          fontSize: 12,
-                          color: Colors.blueAccent,
-                          fontWeight: FontWeight.bold,
-                        ),
-                        recognizer: TapGestureRecognizer()
-                          ..onTap = () {
-                            Navigator.of(context).push(MaterialPageRoute(
-                                builder: (context) => const SignUpPage()));
-                            setState(() {});
-                          })
-                  ])),
-                  const SizedBox(height: 20),
-                  ElevatedButton.icon(
-                    onPressed: () {
-                      setState(() {
-                        isLoading = true;
-                      });
-                      login();
-                      setState(() {
-                        isLoading = false;
-                      });
-                    },
-                    icon: const Icon(
-                      Icons.login_sharp,
-                      color: Colors.black87,
-                    ),
-                    style: ButtonStyle(
-                        shape:
-                            MaterialStateProperty.all<RoundedRectangleBorder>(
-                                RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(18.0),
-                                    side: const BorderSide(
-                                        color: Colors.blueAccent)))),
-                    label: const Text(
+                  child: Column(mainAxisSize: MainAxisSize.min, children: [
+                    Text(
                       "Login",
                       style: TextStyle(
-                        fontFamily: "VareLaRound",
-                        fontSize: 15,
+                        fontSize: 37,
                         fontWeight: FontWeight.bold,
-                        color: Colors.black87,
-                        letterSpacing: 2,
+                        fontFamily: "VareLaRound",
+                        // letterSpacing: 2
                       ),
                     ),
-                  ),
-                ]),
-              ),
+                    const SizedBox(height: 20),
+                    // gmail text feild
+                    TextField(
+                      controller: userGmail,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: Color.fromARGB(255, 66, 125, 145),
+                            ),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0))),
+                        label: Text(
+                          "Email",
+                          style: TextStyle(
+                            fontFamily: "VareLaRound",
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black45,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    // User's password text feild
+                    TextField(
+                      obscureText: true,
+                      controller: userPassword,
+                      decoration: const InputDecoration(
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: Colors.transparent),
+                            borderRadius:
+                                BorderRadius.all(Radius.circular(10.0))),
+                        label: Text(
+                          "Password",
+                          style: TextStyle(
+                            fontFamily: "VareLaRound",
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black45,
+                            // color: Colors.black45,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15,
+                    ),
+                    Text.rich(TextSpan(children: [
+                      const TextSpan(
+                        text: "Don't have an Account?",
+                        style: TextStyle(
+                          fontFamily: "VareLaRound",
+                          fontSize: 12,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      TextSpan(
+                          text: " SingUp",
+                          style: const TextStyle(
+                            fontFamily: "VareLaRound",
+                            fontSize: 12,
+                            color: Colors.blueAccent,
+                            // fontWeight: FontWeight.bold,
+                          ),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              Navigator.of(context).push(MaterialPageRoute(
+                                  builder: (context) => const SignUpPage()));
+                              setState(() {});
+                            })
+                    ])),
+                    const SizedBox(height: 20),
+                    ElevatedButton.icon(
+                      onPressed: () {
+                        setState(() {
+                          isLoading = true;
+                        });
+                        login();
+                        setState(() {
+                          isLoading = false;
+                        });
+                      },
+                      icon: const Icon(
+                        Icons.login_sharp,
+                      ),
+                      style: ButtonStyle(
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                            side: const BorderSide(color: Colors.transparent),
+                          ),
+                        ),
+                      ),
+                      label: const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontFamily: "VareLaRound",
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2,
+                        ),
+                      ),
+                    ),
+                  ]),
+                ),
+              ],
             ),
           );
   }
@@ -181,6 +192,7 @@ class _LoginPageState extends State<LoginPage> {
     try {
       await FirebaseAuth.instance.signInWithEmailAndPassword(
           email: userGmail.text.trim(), password: userPassword.text.trim());
+      setState(() {});
     } on FirebaseAuthException catch (errorMsg) {
       Fluttertoast.showToast(
           msg: "${errorMsg.message}", gravity: ToastGravity.TOP);
