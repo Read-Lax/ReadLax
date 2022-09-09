767b4e12bbda82e3250e05140eec7ef0a9ef02fa
0RaMsY0 <0ramsy0@gmail.com>
2022-09-09 01:04:47 +0100

    Changed app version

--- lib/shared/global.dart
+++ lib/shared/global.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:intl/intl.dart' as intl;
 // app version
-String currentAppVersion = "0.2-beta.7";
+String currentAppVersion = "0.2-beta.8";
 
 // access user data in firebase
 User? user = FirebaseAuth.instance.currentUser;
